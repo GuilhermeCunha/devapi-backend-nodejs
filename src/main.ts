fb7d4a21a8a79df303721040f3731946ebac5ef0
Guilherme Cunha <guiscunha@gmail.com>
2021-04-13 09:28:33 -0300

    feat: adicionando prefixo v1

--- src/main.ts
+++ src/main.ts
@@ -13,7 +13,6 @@ async function bootstrap() {
     .setTitle('DevApi Api')
     .setDescription('DevApi Api')
     .setVersion('0.0.1')
-    .setBasePath('v1')
     .addBearerAuth()
     .build();
 
